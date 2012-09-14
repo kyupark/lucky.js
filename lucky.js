4828395ca9677fd9dd6f6243421434fd920ffb3e
Kyu Park <park@kyu.co>
2012-09-14 17:45:35 -0400

    prettier feeling lucky url

--- lucky.js
+++ lucky.js
@@ -1,11 +1,11 @@
  $(document).ready(function() {
  	$('em').each(function() {
-		var text = String($(this).text()).split('-').join('+');
+		var text = String($(this).text()).split('-').join('').split(' ').join('+');
 		var url = 'http://www.google.com/search?btnI&q=site:youtube.com+' + text;
 		$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
 	});
 	$('strong').each(function() {
-		var text = String($(this).text()).split('-').join('+');
+		var text = String($(this).text()).split('-').join('').split(' ').join('+');
 		var url = 'http://www.google.com/search?btnI&q=' + text;
 		$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
 	});
