d5270de552fb3dddfb2d4a778ca4b22ede0589b1
Kyu Park <park@kyu.co>
2012-09-14 17:54:46 -0400

    open in new tab

--- lucky.js
+++ lucky.js
@@ -2,11 +2,11 @@
  	$('em').each(function() {
 		var text = String($(this).text()).split('-').join('').split(' ').join('+');
 		var url = 'http://www.google.com/search?btnI&q=site:youtube.com+' + text;
-		$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
+		$(this).html('<a target="_blank" href="' + url + '">' + $(this).text() + '</a>');
 	});
 	$('strong').each(function() {
 		var text = String($(this).text()).split('-').join('').split(' ').join('+');
 		var url = 'http://www.google.com/search?btnI&q=' + text;
-		$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
+		$(this).html('<a target="_blank" href="' + url + '">' + $(this).text() + '</a>');
 	});
 });
