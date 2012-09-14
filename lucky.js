3f716c68bbf2557297f8e7c5973dd6825367ca0c
Kyu Park <park@kyu.co>
2012-09-14 17:30:28 -0400

    add doc ready

--- lucky.js
+++ lucky.js
@@ -1,10 +1,12 @@
-$('em').each(function() {
-	var text = String($(this).text()).split(' ').join('+');
-	var url = 'http://www.google.com/search?btnI&q=site:youtube.com+' + text;
-	$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
-});
-$('strong').each(function() {
-	var text = String($(this).text()).split(' ').join('+');
-	var url = 'http://www.google.com/search?btnI&q=' + text;
-	$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
+ $(document).ready(function() {
+ 	$('em').each(function() {
+		var text = String($(this).text()).split(' ').join('+');
+		var url = 'http://www.google.com/search?btnI&q=site:youtube.com+' + text;
+		$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
+	});
+	$('strong').each(function() {
+		var text = String($(this).text()).split(' ').join('+');
+		var url = 'http://www.google.com/search?btnI&q=' + text;
+		$(this).html('<a href="' + url + '">' + $(this).text() + '</a>');
+	});
 });
