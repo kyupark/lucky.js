b9ecb5b4cd74f2c320bbd75a3a32f28d4029f172
Kyu Park <park@kyu.co>
2012-09-15 01:11:18 -0400

    include site support

--- lucky.js
+++ lucky.js
@@ -1,11 +1,22 @@
-function lucky(it) {
+function lucky(it,site) {
+	var url = 'http://www.google.com/search?btnI&q=';
+	if (site ==! undefined) {
+		url = url + "+site" + site + "+";
+
+	}
 	var text = String(it.text()).split('-').join('').split(' ').join('+');
-	var url = 'http://www.google.com/search?btnI&q=' + text;
+	url = url + text;
 	it.html('<a target="_blank" href="' + url + '">' + it.text() + '</a>');
 }
 
 $(document).ready(function() {
- 	$('.cont > li').each(lucky($(this)));
- 	$('.cont > em').each(lucky($(this)));
- 	$('.cont > strong').each(lucky($(this)));
+ 	$('.cont > li').each(function() {
+ 		lucky($(this), "youtube.com");
+	});
+	$('em').each(function() {
+ 		lucky($(this), "youtube.com");
+	});
+	$('strong').each(function() {
+ 		lucky($(this));
+	});
 });
